5f724d13a10313858855870b59f899a54733fa20
Roman Moskalenko <skalermo@gmail.com>
2020-11-07 20:19:08 +0100

    Solve using astar

--- solve.py
+++ solve.py
@@ -1,33 +1,34 @@
-# (1+n)/2 * n - sum of 1..n
-# 0
-# 1 2
-# 3 4 5
-# 6 7 8 9
-# 10 11 12 13
 from Triangle import Triangle
+
+import time
+from PathTree import PathTree, Node
 
 
 def extract_tree_from(filename):
-    tree = []
-    levels = 0
+    triangle = []
     with open(filename, 'r') as fh:
         for line in fh:
-            tree.extend(map(lambda x: int(x), line.split()))
-            levels += 1
-    return tree, levels
+            triangle.append(list(map(lambda x: int(x), line.split())))
+    return triangle
 
 
 def main():
-    tasks = [
+    problems = [
         'very_easy.txt',
         'easy.txt',
-        'medium.txt'
+        'medium.txt',
+        'hard.txt'
     ]
-    tree, levels = extract_tree_from(tasks[2])
-    print(tree)
-    moves, cost = Triangle(tree, levels).find_best_route()
-    print(cost)
-    print(moves)
+    triangle = extract_tree_from(problems[2])
+    tree = PathTree()
+    for nodes in triangle:
+        tree.add_leaves([Node(x) for x in nodes])
+
+    start = time.time()
+    path, cost = tree.find_best_path()
+    end = time.time()
+    print(cost, path)
+    print(f'Time consumed: {end - start}')
 
 
 if __name__ == "__main__":
